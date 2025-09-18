8c74e3e0dd6c59a0a2e794af2fab23a500f4e7e6
Alexander <alexander.bureyko@gmail.com>
2025-09-18 23:23:15 +0300

    feat: add sorting pics by creation date

--- database.py
+++ database.py
@@ -36,11 +36,11 @@ class CatDataBase:
                 logger.error(f"error adding favourite: {e}")
                 return False
             
-    async def get_user_favourites(self, user_id):
+    async def get_user_favourites(self, user_id, limit = 5, offset=0):
         async with aiosqlite.connect(self.db_path) as conn:
             try:
                 cursor = await conn.cursor()
-                await cursor.execute('''SELECT file_id FROM favourites WHERE user_id = ?''', (user_id,))
+                await cursor.execute('''SELECT file_id FROM favourites WHERE user_id = ? ORDER BY id DESC LIMIT ? OFFSET ?''', (user_id, limit, offset))
                 results = await cursor.fetchall()
                 return [row[0] for row in results] if results else []
             except Exception as e:
